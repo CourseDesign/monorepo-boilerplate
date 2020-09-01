da87278e14c355bde68856ff8a0c63d7ac7148ce
siyual-park <siyual.bak@gmail.com>
2020-09-01 19:46:21 +0900

    feature/build_cross_env

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,15 @@
 const gulp = require('gulp');
+const ts = require('gulp-typescript');
 
-gulp.task('default', () => {
+const tsProject = ts.createProject(
+  process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
+);
+
+gulp.task('scripts', () => {
+  return tsProject.src()
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest("dist")
+    );
 });
+
