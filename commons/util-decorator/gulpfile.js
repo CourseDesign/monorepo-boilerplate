b49b5b8a5077c36abc4a2b19e9604ea6548dc302
siyual.park <siyual.bak@gmail.com>
2021-03-05 23:36:35 +0900

    lint

--- commons/util-decorator/gulpfile.js
+++ commons/util-decorator/gulpfile.js
@@ -4,19 +4,19 @@ const ts = require('gulp-typescript');
 const uglify = require('gulp-uglify');
 
 const tsProject = ts.createProject(
-    process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
+  process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
 );
 
 gulp.task('build', () => {
   return tsProject.src()
-      .pipe(tsProject())
-      .pipe(gulp.dest('dist'));
+    .pipe(tsProject())
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('uglify', () => {
   return gulp.src('dist/**/*.js')
-      .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
-      .pipe(gulp.dest('dist'));
+    .pipe(gulpif(process.env.NODE_ENV === 'production', uglify()))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', gulp.series('build', 'uglify'));
