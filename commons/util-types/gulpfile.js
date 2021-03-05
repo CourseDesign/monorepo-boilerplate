7ff879e09288f1188ef4efe06608acdbf37082aa
siyual.park <siyual.bak@gmail.com>
2021-03-05 23:19:04 +0900

    refactoring

--- commons/util-types/gulpfile.js
+++ commons/util-types/gulpfile.js
@@ -2,12 +2,13 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject(
-  process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
+    process.env.NODE_ENV ? `tsconfig.${process.env.NODE_ENV.toLowerCase()}.json` : 'tsconfig.json'
 );
 
-gulp.task('default', () => {
+gulp.task('build', () => {
   return tsProject.src()
-    .pipe(tsProject())
-    .pipe(gulp.dest('dist'));
+      .pipe(tsProject())
+      .pipe(gulp.dest('dist'));
 });
 
+gulp.task('default', gulp.series("build"));
